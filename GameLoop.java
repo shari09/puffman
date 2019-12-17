dbced995542130dbf51d5cb5f8d2a0f521510a4f
Shari Sun <shari09cxxcr@gmail.com>
2019-12-16 20:28:49 -0500

    empty files

---
+++ GameLoop.java
@@ -0,0 +1,9 @@
+import java.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class GameLoop implements KeyListener {
+  public static void main(String[] args) {
+    World world = new World();
+  }
+}
---
+++ World.java
@@ -0,0 +1,32 @@
+import java.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class World extends JFrame {
+  private int gravity = 8;
+  private int margin = 30;
+
+  public World() {
+    this.setSize(1000, 800);
+    this.setLayout(null);
+    this.setVisible(true);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    this.setFocusable(true);
+  }
+
+  ArrayList<Item> allItems;
+  Character[] players = new Character[2];
+  Block[] blocks = new Block[7];
+
+  /**
+   * gets the gravity constant of this world
+   * @return int, the world's gravity
+   */
+  public int getGravity() {
+    return this.gravity;
+  }
+  
+  
+
+}
